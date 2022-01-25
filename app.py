9c485ae5dbba9e529c750d04c16c17da864026bc
bisrat-negash <negashbisre@gmail.com>
2022-01-25 14:37:41 +0300

    third - commit

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+app = Flask(__name__)
 
-app=Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index(): 
     return render_template('index.html')
 
 
-@app.route("/login")
-def login(): 
+@app.route('/login')
+def login():
     return render_template('login.html')
 
-@app.route("/signup")
+
+@app.route('/signup')
 def signup(): 
     return render_template('signup.html')
 
+
 if __name__ == '__main__':
-      app.run(debug=True)
+ app.run(debug=True, port=5001)
+
